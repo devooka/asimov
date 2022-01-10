3a0888582638f220920a58df4cbfa097ec8cc1d1
Julio <85838257+juliobsl@users.noreply.github.com>
2022-01-10 15:46:25 -0300

    Redirecionamentos feitos e outros testes
    Fazer controle de load de noticias no php exibindo a última notícia após a última cadastrada.

--- main.js
+++ main.js
@@ -15,21 +15,37 @@
     const urlDados = 'http://goiatins.localhost:8080/admin/php/escolas/loadEscolas.php?p=educacao';
     var arrDados;
 
+    var formulario = document.getElementsByClassName("we-form")[0];
+
      fetch(urlDados)
         .then((resp) => resp.json())
         .then(function(data) {
            arrDados = data;
 
          console.log(arrDados);
-         arrDados.forEach(function (item,index){
-            preencheForm(item);
-         });
+
+         var pagina = document.getElementsByTagName("BODY")[0].innerHTML.toString();
+         var encontra = 'redirect';
+         console.log(pagina);
+         console.log(pagina.indexOf(encontra));
+         if(pagina.indexOf(encontra)>-1){
+             console.log("Notícia enviada!");
+             window.open('https://www.carmolandia.to.gov.br/admin/posts/home/-1/-1/all/all/all/1','_parent');
+             return;
+         }
+
+         preencheForm(arrDados[0]);
+         console.log("Enviando...");
+         formulario.submit();
+
 
 
     })
         .catch(function(error) {
         console.log(error);
     });
+
+
     function preencheForm(item){
 
         var inputs = document.getElementsByTagName("TEXTAREA");
@@ -41,12 +57,41 @@
         //console.log(subtitulo);
         subtitulo.value = item.endereco;
 
+        anexarImagem(item);
+
+        var editor = document.getElementsByClassName("mce")[0];
+        editor.value= item.telefoneprincipal;
+
+        var datapubli = document.getElementsByName("date_at")[0];
+        datapubli.value = "10/01/2021 00:00";
+
+        var selcat = document.getElementsByName("category_id")[0];
+        //console.log(selcat);
+        selcat.value=2;
+
+        var status = document.getElementsByName("status")[0];
+        //console.log(selcat);
+        status.value='active';
+
+
     }
 
-   var inp = document.getElementsByName("cover")[0];
+    function anexarImagem(item){
 
+         var inp = document.getElementsByName("cover")[0];
+         let url = 'http://goiatins.localhost:8080/admin/assets/images/logos-muns/goiatins/1.png';
+         toDataURL(url)
+             .then(dataUrl => {
+             //console.log('Here is Base64 Url', dataUrl)
+             var fileData = dataURLtoFile(dataUrl, "imagem-noticia-"+item.id+".jpg");
+             //console.log("Here is JavaScript File Object",fileData)
+             const dT = new DataTransfer();
+             dT.items.add(fileData);
+             inp.files = dT.files;
+         })
 
-    let url = 'http://goiatins.localhost:8080/admin/assets/images/logos-muns/goiatins/1.png'
+    }
+
     const toDataURL = url => fetch(url)
       .then(response => response.blob())
       .then(blob => new Promise((resolve, reject) => {
@@ -66,32 +111,6 @@
      }
    return new File([u8arr], filename, {type:mime});
   }
-
-  toDataURL(url)
-  .then(dataUrl => {
-     //console.log('Here is Base64 Url', dataUrl)
-     var fileData = dataURLtoFile(dataUrl, "imageName.jpg");
-     //console.log("Here is JavaScript File Object",fileData)
-      const dT = new DataTransfer();
-      dT.items.add(fileData);
-      inp.files = dT.files;
-   })
-
-
-    var editor = document.getElementsByClassName("mce")[0];
-    editor.value="Lorem impsum dolor";
-
-    var datapubli = document.getElementsByName("date_at")[0];
-    datapubli.value = "10/01/2021 00:00";
-
-    var selcat = document.getElementsByName("category_id")[0];
-    //console.log(selcat);
-    selcat.value=2;
-
-    var status = document.getElementsByName("status")[0];
-    //console.log(selcat);
-    status.value='active';
-
 
 
 
